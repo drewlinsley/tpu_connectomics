d736ec5f8a9137e90efa9ebafc1edeff39454464
Frederick Rice <freddie.rice@gmail.com>
2018-08-15 19:52:56 +0000

    added the components

--- ops/initialization.py
+++ ops/initialization.py
@@ -14,10 +14,6 @@
 # ==============================================================================
 """Weight initializers for use with layers."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import math
 
 from tensorflow.python.framework import dtypes
